ae76986c91b8e372eba5a3ba8157e0acf5117177
Zdenek Vostrel <zdenek.vostrel@cern.ch>
2021-11-23 22:31:19 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='Simion_package',
+   version='0.9.1',
+   description='A simple module for creating complex Simion gem files and possibly easily rescaling them.',
+   author='Zdenek Vostrel',
+   packages=['Simion_package'],
+   install_requires=['typing'],
+)
